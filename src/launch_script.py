1eab86335aeec20e5e1184ae8c19ee31629f4460
John Torcivia <torcivia@gwu.edu>
2018-08-03 00:24:26 +0000

    fixed argparse issue

--- src/launch_script.py
+++ src/launch_script.py
@@ -173,7 +173,7 @@ def generate_sbatch_scripts(callers, **kwargs):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Commands for launch script.')
-    parser.add_argument('ip', dest='ip', help='The IP address of the server, in format xxx.xxx.xxx.xxx.  This is required.')
+    parser.add_argument('ip', help='The IP address of the server, in format xxx.xxx.xxx.xxx.  This is required.')
     parser.add_argument('--base_dir', '-b', dest='base_dir', default='/home/torcivia/tcga/', help='Changes the base directory that computation scripts should be generated.  Default is /home/torcivia/tcga/.')
     parser.add_argument('--verbose', '-v', dest='verbose', action='store_true', help="Turns on verbosity.")
     # parser.add_argument('--quick', '-q', dest='quick', action='store_true', help='Instead of re-generating the pairing index, reads from matches.csv (if exists).')
