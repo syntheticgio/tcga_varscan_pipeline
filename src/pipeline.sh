adc836d1fd454c74daf80cef367563b67a4540d8
John "Gio" Torcivia <torcivia@gwmail.gwu.edu>
2019-06-12 16:42:14 -0600

    Comment cleanup; updated to make IP server argument optional (will not push to server if it isn't given).
    Signed-off-by: John "Gio" Torcivia <torcivia@gwmail.gwu.edu>

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -16,12 +16,12 @@
 #
 if [[ $# -lt 5 ]] ; then
     echo 'The pipeline script requires five arguments with an optional sixth one:'
-	echo "pipeline.h </path/to/normal.bam> </path/to/tumor.bam> <output bucket location (i.e. gs://my-bucket/output)> <output base name> <reference> <ip address of database>"
+	echo "pipeline.h </path/to/normal.bam> </path/to/tumor.bam> <output bucket location (i.e. gs://my-bucket/output)> <output base name> <reference> <[optional] ip address of database>"
     exit 1
 fi
 if [[ $# -gt 6 ]] ; then
-    echo 'The pipeline script requires six arguments:'
-	echo "pipeline.h </path/to/normal.bam> </path/to/tumor.bam> <output bucket location (i.e. gs://my-bucket/output)> <output base name> <reference> <ip address of database>"
+    echo 'The pipeline script requires five arguments with an optional sixth:'
+	echo "pipeline.h </path/to/normal.bam> </path/to/tumor.bam> <output bucket location (i.e. gs://my-bucket/output)> <output base name> <reference> <[optional] ip address of database>"
     exit 1
 fi
 
@@ -31,10 +31,8 @@ fi
 if [[ $1 == "-h" ]]; then
 	echo "Help"
 	echo "-------------"
-	echo "pipeline.h </path/to/normal.bam> </path/to/tumor.bam> <output bucket location (i.e. gs://my-bucket/output)> <output base name> <reference> <ip address of database>"
+	echo "pipeline.h </path/to/normal.bam> </path/to/tumor.bam> <output bucket location (i.e. gs://my-bucket/output)> <output base name> <reference> <[optional] ip address of database>"
 	echo ""
-	echo "The files should be locations the container can access; this script is set up to accept accessible gs:// locations and copy them in using gsutil."
-	echo "For example: gs://my-bucket/normal.bam"
 	echo "Output location should have the complete gs:// path up to (but excluding) the file name"
 	exit 1
 fi
@@ -85,95 +83,31 @@ mkdir -p OUTPUT
 
 
 #
-# This is the base formating option that all tables take.  This assumes the beginning hasn't been defined yet
+# This is the base formating option that all tables take in the database.  This assumes the beginning hasn't been defined yet
 #
 TIME_FORMAT="\"CommandLineArguments\":\"%C\",\"AvgSizeUnsharedDataArea_KBs\":\"%D\",\"ElapsedTime_s\":\"%E\",\"NumPageFaults\":\"%F\",\"NumFileSystemInputs\":\"%I\",\"AvgMemUse_KBs\":\"%K\",\"MaxResidentSetSize_KBs\":\"%M\",\"NumFileSystemOutputs\":\"%O\",\"CPU_Percent\":\"%P\",\"NumRecoverablePageFaults\":\"%R\",\"CPUUsedInKernelMode_s\":\"%S\",\"CPUUsedInUserMode_s\":\"%U\",\"TimesProcessSwappedOutOfMainMemory\":\"%W\",\"AverageAmountSharedText\":\"%X\",\"SystemPageSize_KBs\":\"%Z\",\"TimesProcessContextSwitched\":\"%c\",\"ElapsedRealTimeUsed_s\":\"%e\",\"NumSignalsDelivered\":\"%k\",\"AverageUnsharedStackSize_KBs\":\"%p\",\"NumSocketMessagesReceived\":\"%r\",\"NumSocketMessagesSent\":\"%s\",\"ResidentSetSize_KBs\":\"%t\",\"NumTimesContextSwitchedVoluntarily\":\"%w\",\"ExitStatus\":\"%x\"}"
 
 # TODO : check to make sure that the stderr redirect doesn't capture time output also.
 
-#
-# Create the full format option for the SORT command - prepend sort specific string onto TIME_FORMAT
-#
-SORT_FORMAT="{\"ID\":\"${NORMAL_BAM}\",${TIME_FORMAT}"
+######################################################
+# Stages 1 and 2 were completed in the split_by_ref.sh
+######################################################
 
-#
-# Run the SORT command for NORMAL and then submit the database
-# Only run if the bai index is not included
-# Stage 1
-#
-# echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":1}" > OUTPUT/running_entry.txt
-# python post_json.py -u createrunningsample -v -i ${IP} -f OUTPUT/running_entry.txt
-# echo "=========================================================="
-# echo "1. SORTING:  /usr/bin/time -o OUTPUT/samtools_sort_normal_time.txt --format "${SORT_FORMAT}" ${SAMTOOLS} sort ${NORMAL_BAM} -o sorted_${NORMAL_BAM} 1> OUTPUT/samtools_sort_normal.stdout 2> OUTPUT/samtools_sort_normal.stderr"
-# if [ ${NORMAL_SORTED} -gt 0 ]
-# then
-#     /usr/bin/time -o OUTPUT/samtools_sort_normal_time.txt --format "${SORT_FORMAT}" ${SAMTOOLS} sort ${NORMAL_BAM} -o sorted_${NORMAL_BAM} 1> OUTPUT/samtools_sort_normal.stdout 2> OUTPUT/samtools_sort_normal.stderr
-#     SORT_NORMAL_ERROR_CODE=$?
-#     echo -e "\tERROR CODE: ${SORT_NORMAL_ERROR_CODE}"
-# else
-#     echo "Already sorted.  Simulating stdout and stderr for logging."
-#     mv ${NORMAL_BAM} sorted_${NORMAL_BAM}
-#     cp ${NORMAL_BAM}.bai sorted_${NORMAL_BAM}.bai
-#     /usr/bin/time -o OUTPUT/samtools_sort_normal_time.txt --format "${SORT_FORMAT}" ls 1> OUTPUT/samtools_sort_normal.stdout 2> OUTPUT/samtools_sort_normal.stderr
-#     SORT_NORMAL_ERROR_CODE=$?
-#     echo -e "\tERROR CODE: ${SORT_NORMAL_ERROR_CODE}"
-# fi
-# echo ""
-# echo "2. POSTing time data to database: python post_json.py -u samtoolssort -f OUTPUT/samtools_sort_normal_time.txt -v -i ${IP}"
-# python post_json.py -u samtoolssort -f OUTPUT/samtools_sort_normal_time.txt -v -i ${IP}
-# POST_NORMAL_ERROR_CODE=$?
-# echo -e "\tERROR CODE: ${POST_NORMAL_ERROR_CODE}"
-# echo ""
-# echo "STDERR"
-# echo "------"
-# cat OUTPUT/samtools_sort_normal.stderr
-# echo "=========================================================="
-
-#
-# Run the SORT command for TUMOR and submit to the database
-# Stage 2
-#
-# echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":2}" > OUTPUT/running_entry.txt
-# python post_json.py -u updaterunningsample -v -i ${IP} -f OUTPUT/running_entry.txt
-
-# echo ""
-# echo "3. SORTING: /usr/bin/time -o OUTPUT/samtools_sort_tumor_time.txt --format "${SORT_FORMAT}" ${SAMTOOLS} sort ${TUMOR_BAM} -o sorted_${TUMOR_BAM} 1> OUTPUT/samtools_sort_tumor.stdout 2> OUTPUT/samtools_sort_tumor.stderr"
-# if [ ${TUMOR_SORTED} -gt 0 ]
-# then
-#     /usr/bin/time -o OUTPUT/samtools_sort_tumor_time.txt --format "${SORT_FORMAT}" ${SAMTOOLS} sort ${TUMOR_BAM} -o sorted_${TUMOR_BAM} 1> OUTPUT/samtools_sort_tumor.stdout 2> OUTPUT/samtools_sort_tumor.stderr
-#     SORT_TUMOR_ERROR_CODE=$?
-#     echo -e "\tERROR CODE: ${SORT_TUMOR_ERROR_CODE}"
-# else
-#     echo "Already sorted.  Simulating stdout and stderr for logging."
-#     mv ${TUMOR_BAM} sorted_${TUMOR_BAM}
-#     cp ${TUMOR_BAM}.bai sorted_${TUMOR_BAM}.bai
-#     /usr/bin/time -o OUTPUT/samtools_sort_tumor_time.txt --format "${SORT_FORMAT}" ls 1> OUTPUT/samtools_sort_tumor.stdout 2> OUTPUT/samtools_sort_tumor.stderr
-#     SORT_TUMOR_ERROR_CODE=$?
-#     echo -e "\tERROR CODE: ${SORT_TUMOR_ERROR_CODE}"
-# fi
-# echo ""
-# echo "4. POSTing time data to database: python post_json.py -u samtoolssort -f OUTPUT/samtools_sort_tumor_time.txt -v -i ${IP}"
-# python post_json.py -u samtoolssort -f OUTPUT/samtools_sort_tumor_time.txt -v -i ${IP}
-# POST_TUMOR_ERROR_CODE=$?
-# echo -e "\tERROR CODE: ${POST_TUMOR_ERROR_CODE}"
-# echo ""
-# echo "STDERR"
-# echo "------"
-# cat OUTPUT/samtools_sort_tumor.stderr
-# echo "=========================================================="
-
-#
+######################################################
 # Determine MPILEUP time output format
-#
+######################################################
 MPILEUP_FORMAT="{\"NormalID\":\"${NORMAL_BAM}\",\"TumorID\":\"${TUMOR_BAM}\",\"Reference\":\"${REFERENCE}\",${TIME_FORMAT}"
 
-#
+######################################################
 # Run samtools mpileup
 # Stage 3
-#
+######################################################
 # TODO: Need to update here to have the sorted_*BAM files with the proper _REF_xxx appended
 echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":3,\"Reference\":\"${REFERENCE_NAME}\"}" > OUTPUT/running_entry.txt
-python post_json.py -u createrunningsample -v -i ${IP} -f OUTPUT/running_entry.txt 2> "__createsample_${REFERENCE_NAME}_post.stderr"
+if [[ ! -z "$3" ]]
+then
+    python post_json.py -u createrunningsample -v -i ${IP} -f OUTPUT/running_entry.txt 2> "__createsample_${REFERENCE_NAME}_post.stderr"
+fi
 echo ""
 MPILEUP_OUTPUT_NAME="intermediate_mpileup.${REFERENCE_NAME}.pileup"
 MPILEUP_OUTPUT_STDERR="intermediate_mpileup.${REFERENCE_NAME}.stderr"
@@ -192,20 +126,23 @@ echo "5. MPILEUP: /usr/bin/time -o OUTPUT/${MPILEUP_OUTPUT_TIME} --format ${MPIL
 MPILEUP_ERROR_CODE=$?
 echo -e "\tERROR CODE: ${MPILEUP_ERROR_CODE}"
 echo ""
-echo "6. POSTing mpileup: python post_json.py -u mpileup -f OUTPUT/${MPILEUP_OUTPUT_TIME} -v -i ${IP}"
-python post_json.py -u mpileup -f OUTPUT/${MPILEUP_OUTPUT_TIME} -v -i ${IP} 2> "__mpileup_${REFERENCE_NAME}_post.stderr"
-POST_MPILEUP_ERROR_CODE=$?
-echo -e "\tERROR CODE: ${POST_MPILEUP_ERROR_CODE}"
-echo ""
+if [[ ! -z "$3" ]]
+then
+    echo "6. POSTing mpileup: python post_json.py -u mpileup -f OUTPUT/${MPILEUP_OUTPUT_TIME} -v -i ${IP}"
+    python post_json.py -u mpileup -f OUTPUT/${MPILEUP_OUTPUT_TIME} -v -i ${IP} 2> "__mpileup_${REFERENCE_NAME}_post.stderr"
+    POST_MPILEUP_ERROR_CODE=$?
+    echo -e "\tERROR CODE: ${POST_MPILEUP_ERROR_CODE}"
+    echo ""
+fi
 echo "STDERR"
 echo "------"
 cat OUTPUT/${MPILEUP_OUTPUT_STDERR}
 echo "=========================================================="
 
-#
+######################################################
 # Get base somatic mutations
 # Stage 4
-#
+######################################################
 BASE_OUTPUT_NAME="${BASE_OUTPUT_NAME}_${REFERENCE_NAME%.*}"
 VARSCAN_SOMATIC_STDERR="varscan_somatic_${REFERENCE_NAME%.*}.stderr"
 VARSCAN_SOMATIC_TIME="somatic_varscan_time_${REFERENCE_NAME%.*}.txt"
@@ -234,20 +171,23 @@ echo "7. VARSCAN SOMATIC: /usr/bin/time -o OUTPUT/${VARSCAN_SOMATIC_TIME} --form
 VARSCAN_ERROR_CODE=$?
 echo -e "\tERROR CODE: ${VARSCAN_ERROR_CODE}"
 echo ""
-echo "8. POSTing varscan: python post_json.py -u varscansomatic -f OUTPUT/${VARSCAN_SOMATIC_TIME} -v -i ${IP}"
-python post_json.py -u varscansomatic -f OUTPUT/${VARSCAN_SOMATIC_TIME} -v -i ${IP} 2> "__varscansomatic_${REFERENCE_NAME}_post.stderr"
-POST_VARSCAN_ERROR_CODE=$?
-echo -e "\tERROR CODE: ${POST_VARSCAN_ERROR_CODE}"
-echo ""
+if [[ ! -z "$3" ]]
+then
+    echo "8. POSTing varscan: python post_json.py -u varscansomatic -f OUTPUT/${VARSCAN_SOMATIC_TIME} -v -i ${IP}"
+    python post_json.py -u varscansomatic -f OUTPUT/${VARSCAN_SOMATIC_TIME} -v -i ${IP} 2> "__varscansomatic_${REFERENCE_NAME}_post.stderr"
+    POST_VARSCAN_ERROR_CODE=$?
+    echo -e "\tERROR CODE: ${POST_VARSCAN_ERROR_CODE}"
+    echo ""
+fi
 echo "STDERR"
 echo "------"
 cat OUTPUT/${VARSCAN_SOMATIC_STDERR}
 echo "=========================================================="
 
-#
+######################################################
 # Process for somatic SNPs
 # Stage 5
-#
+######################################################
 VARSCAN_SOMATIC_SNP_TIME="process_somatic_snp_time_${REFERENCE_NAME%.*}.txt"
 VARSCAN_SOMATIC_SNP_STDERR="process_somatic_snp_${REFERENCE_NAME%.*}.stderr"
 echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":5,\"Reference\":\"${REFERENCE_NAME}\"}" > OUTPUT/running_entry.txt
@@ -264,20 +204,23 @@ echo "9. VARSCAN SOMATIC SNP CALLS: /usr/bin/time -o OUTPUT/${VARSCAN_SOMATIC_SN
 VARSCAN_SNP_ERROR_CODE=$?
 echo -e "\tERROR CODE: ${VARSCAN_SNP_ERROR_CODE}"
 echo ""
-echo "10. POSTing VARSCAN SNP: python post_json.py -u varscanprocesssomaticsnps -f OUTPUT/${VARSCAN_SOMATIC_SNP_TIME} -v -i ${IP}"
-python post_json.py -u varscanprocesssomaticsnps -f OUTPUT/${VARSCAN_SOMATIC_SNP_TIME} -v -i ${IP} 2> "__varscanprocesssomaticsnps_${REFERENCE_NAME}_post.stderr"
-POST_VARSCAN_SNP_ERROR_CODE=$?
-echo -e "\tERROR CODE: ${POST_VARSCAN_SNP_ERROR_CODE}"
-echo ""
+if [[ ! -z "$3" ]]
+then
+    echo "10. POSTing VARSCAN SNP: python post_json.py -u varscanprocesssomaticsnps -f OUTPUT/${VARSCAN_SOMATIC_SNP_TIME} -v -i ${IP}"
+    python post_json.py -u varscanprocesssomaticsnps -f OUTPUT/${VARSCAN_SOMATIC_SNP_TIME} -v -i ${IP} 2> "__varscanprocesssomaticsnps_${REFERENCE_NAME}_post.stderr"
+    POST_VARSCAN_SNP_ERROR_CODE=$?
+    echo -e "\tERROR CODE: ${POST_VARSCAN_SNP_ERROR_CODE}"
+    echo ""
+fi
 echo "STDERR"
 echo "------"
 cat OUTPUT/${VARSCAN_SOMATIC_SNP_STDERR}
 echo "=========================================================="
 
-#
+######################################################
 # Process for somatic indels
 # Stage 6
-#
+######################################################
 VARSCAN_SOMATIC_INDEL_TIME="process_somatic_indel_time_${REFERENCE_NAME%.*}.txt"
 VARSCAN_SOMATIC_INDEL_STDERR="process_somatic_indel_${REFERENCE_NAME%.*}.stderr"
 echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":6,\"Reference\":\"${REFERENCE_NAME}\"}" > OUTPUT/running_entry.txt
@@ -294,57 +237,27 @@ echo "11. VARSCAN SOMATIC INDEL CALLS: /usr/bin/time -o OUTPUT/${VARSCAN_SOMATIC
 VARSCAN_INDEL_ERROR_CODE=$?
 echo -e "\tERROR CODE: ${VARSCAN_INDEL_ERROR_CODE}"
 echo ""
-echo "12. POSTing VARSCAN INDEL: python post_json.py -u varscanprocesssomaticindels -f OUTPUT/${VARSCAN_SOMATIC_INDEL_TIME} -v -i ${IP}"
-python post_json.py -u varscanprocesssomaticindels -f OUTPUT/${VARSCAN_SOMATIC_INDEL_TIME} -v -i ${IP} 2> "__varscanprocesssomaticindels_${REFERENCE_NAME}_post.stderr"
-POST_VARSCAN_INDEL_ERROR_CODE=$?
-echo -e "\tERROR CODE: ${POST_VARSCAN_INDEL_ERROR_CODE}"
-echo ""
+if [[ ! -z "$3" ]]
+then
+    echo "12. POSTing VARSCAN INDEL: python post_json.py -u varscanprocesssomaticindels -f OUTPUT/${VARSCAN_SOMATIC_INDEL_TIME} -v -i ${IP}"
+    python post_json.py -u varscanprocesssomaticindels -f OUTPUT/${VARSCAN_SOMATIC_INDEL_TIME} -v -i ${IP} 2> "__varscanprocesssomaticindels_${REFERENCE_NAME}_post.stderr"
+    POST_VARSCAN_INDEL_ERROR_CODE=$?
+    echo -e "\tERROR CODE: ${POST_VARSCAN_INDEL_ERROR_CODE}"
+    echo ""
+fi
 echo "STDERR"
 echo "------"
 cat OUTPUT/${VARSCAN_SOMATIC_INDEL_STDERR}
 echo "=========================================================="
 
+#############################################
+# Finished with this leg
+#############################################
 
-# Need to move the output here from ./output (?) to shared mount
-# What is the prefix we should have so far?
-# Don't need to use volumes since we're using buckets (???)
-# 1. Create output directory based on patient ID / TCGA number
-# 2. Copy appropriate files into that directory 
-
-#mv ..indel                  ..indel.Germline.vcf  ..indel.LOH.vcf         ..indel.Somatic.vcf  ..snp                  ..snp.Germline.vcf  ..snp.LOH.vcf         ..snp.Somatic.vcf
-#..indel.Germline.hc.vcf  ..indel.LOH.hc.vcf    ..indel.Somatic.hc.vcf  ..indel.vcf          ..snp.Germline.hc.vcf  ..snp.LOH.hc.vcf    ..snp.Somatic.hc.vcf  ..snp.vcf
-
-# Stage 7
 echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":7,\"Reference\":\"${REFERENCE_NAME}\"}" > OUTPUT/running_entry.txt
-python post_json.py -u updaterunningsample -v -i ${IP} -f OUTPUT/running_entry.txt 2> "__update4_${REFERENCE_NAME}_post.stderr"
-
-# echo ""
-# echo "Compressing VCF files...."
-# tar -zcf ${BASE_OUTPUT_NAME}.vcf.tar.gz *.vcf
-
-# # How to move into GS bucket?
-# # Where to move into bucket?
-
-# echo ""
-# echo "Copying files into the appropriate output location..."
-# gsutil cp ${BASE_OUTPUT_NAME}*.vcf.gz ${OUTPUT_LOCATION}/varscan_results/
-# gsutil cp OUTPUT/* ${OUTPUT_LOCATION}/varscan_results/${BASE_OUTPUT_NAME}/
-
-
-# ERROR_FILES="${OUTPUT_LOCATION}/varscan_results/${BASE_OUTPUT_NAME}/"
-# JSON_FINISHED="{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"errorfiles\":\"${ERROR_FILES}\",\"SortNormalError\":${SORT_NORMAL_ERROR_CODE},\"PostSortNormalError\":${POST_NORMAL_ERROR_CODE},\"SortTumorError\":${SORT_TUMOR_ERROR_CODE},\"PostSortTumorError\":${POST_TUMOR_ERROR_CODE},\"MpileupError\":${MPILEUP_ERROR_CODE},\"PostMpileupError\":${POST_MPILEUP_ERROR_CODE},\"VarscanError\":${VARSCAN_ERROR_CODE},\"PostVarscanError\":${POST_VARSCAN_ERROR_CODE},\"VarscanSnpError\":${VARSCAN_SNP_ERROR_CODE},\"PostVarscanSnpError\":${POST_VARSCAN_SNP_ERROR_CODE},\"VarscanIndelError\":${VARSCAN_INDEL_ERROR_CODE},\"PostVarscanIndelError\":${POST_VARSCAN_INDEL_ERROR_CODE}}"
-# echo "============================================="
-# echo ${JSON_FINISHED} > OUTPUT/finished.txt
-# echo "FINISHED File:"
-# cat OUTPUT/finished.txt
-# echo ""
-# echo ""
-# echo "POSTing job information ..."
-# python post_json.py -u recordfinished -i ${IP} -f OUTPUT/finished.txt
-# # Update Running Entry to finished
-# echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":9,\"Reference\":\"${REFERENCE_NAME}\"}" > OUTPUT/running_entry.txt
-# python post_json.py -u updaterunningsample -v -i ${IP} -f OUTPUT/running_entry.txt
-# echo ""
-# echo "FINISHED!"
+if [[ ! -z "$3" ]]
+then
+    python post_json.py -u updaterunningsample -v -i ${IP} -f OUTPUT/running_entry.txt 2> "__update4_${REFERENCE_NAME}_post.stderr"
+fi
 
 return 0
