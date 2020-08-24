a13c09fed2cb766d00beb2b1dd39608595669512
John Rodriguez <syntheticgio@aol.com>
2020-08-23 23:19:08 -0400

    Fix output of warning (hopefully)
    Signed-off-by: John Rodriguez <syntheticgio@aol.com>

--- src/split_by_ref.sh
+++ src/split_by_ref.sh
@@ -117,6 +117,8 @@ else
     echo "Already sorted.  Simulating stdout and stderr for logging."
     mv "${NORMAL_BAM}" sorted_"${NORMAL_BAM}"
     cp "${NORMAL_BAM}".bai sorted_"${NORMAL_BAM}".bai
+    sleep 60
+    touch sorted_"${NORMAL_BAM}".bai
     # Make simulated time output file for submission
     /usr/bin/time -o OUTPUT/samtools_sort_normal_time.txt --format "${SORT_FORMAT}" ls 1> OUTPUT/samtools_sort_normal.stdout 2> OUTPUT/samtools_sort_normal.stderr
     SORT_NORMAL_ERROR_CODE=$?
@@ -125,7 +127,7 @@ fi
 
 echo ""
 echo "2. POSTing time data to database: python post_json.py -u samtoolssort -f OUTPUT/samtools_sort_normal_time.txt -v -i ${3}"
-if [[ ! -z "$3" ]]
+if [[ -n "$3" ]]
 then
     python post_json.py -u samtoolssort -f OUTPUT/samtools_sort_normal_time.txt -v -i ${3}
 fi
@@ -154,23 +156,25 @@ bamtools split -in sorted_${NORMAL_BAM} -reference
 # Stage 2
 #
 echo "{\"Normal\":\"${NORMAL_BAM}\",\"Tumor\":\"${TUMOR_BAM}\",\"Stage\":2,\"Reference\":\"Human\"}" > OUTPUT/running_entry.txt
-if [[ ! -z "$3" ]]
+if [[ -n "$3" ]]
 then
-    python post_json.py -u updaterunningsample -v -i ${3} -f OUTPUT/running_entry.txt
+    python post_json.py -u updaterunningsample -v -i "${3}" -f OUTPUT/running_entry.txt
 fi
 echo ""
 echo "3. SORTING: /usr/bin/time -o OUTPUT/samtools_sort_tumor_time.txt --format "${SORT_FORMAT}" ${SAMTOOLS} sort ${TUMOR_BAM} -o sorted_${TUMOR_BAM} 1> OUTPUT/samtools_sort_tumor.stdout 2> OUTPUT/samtools_sort_tumor.stderr"
 if [[ ${TUMOR_SORTED} -gt 0 ]]
 then
     echo "Tumor not yet sorted, sorting now..."
-    /usr/bin/time -o OUTPUT/samtools_sort_tumor_time.txt --format "${SORT_FORMAT}" ${SAMTOOLS} sort ${TUMOR_BAM} -o sorted_${TUMOR_BAM} 1> OUTPUT/samtools_sort_tumor.stdout 2> OUTPUT/samtools_sort_tumor.stderr
-    rm ${TUMOR_BAM}
+    /usr/bin/time -o OUTPUT/samtools_sort_tumor_time.txt --format "${SORT_FORMAT}" ${SAMTOOLS} sort "${TUMOR_BAM}" -o sorted_"${TUMOR_BAM}" 1> OUTPUT/samtools_sort_tumor.stdout 2> OUTPUT/samtools_sort_tumor.stderr
+    rm "${TUMOR_BAM}"
     SORT_TUMOR_ERROR_CODE=$?
     echo -e "\tERROR CODE: ${SORT_TUMOR_ERROR_CODE}"
 else
     echo "Already sorted.  Simulating stdout and stderr for logging."
-    mv ${TUMOR_BAM} sorted_${TUMOR_BAM}
-    cp ${TUMOR_BAM}.bai sorted_${TUMOR_BAM}.bai
+    mv "${TUMOR_BAM}" sorted_"${TUMOR_BAM}"
+    cp "${TUMOR_BAM}".bai sorted_"${TUMOR_BAM}".bai
+    sleep 60
+    touch sorted_"${TUMOR_BAM}".bai
     /usr/bin/time -o OUTPUT/samtools_sort_tumor_time.txt --format "${SORT_FORMAT}" ls 1> OUTPUT/samtools_sort_tumor.stdout 2> OUTPUT/samtools_sort_tumor.stderr
     SORT_TUMOR_ERROR_CODE=$?
     echo -e "\tERROR CODE: ${SORT_TUMOR_ERROR_CODE}"
