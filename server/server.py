d74a5c894a81bfb2790a32d6d03b3d90ba00aaa1
John Rodriguez <syntheticgio@aol.com>
2020-08-25 00:54:11 -0400

    Fixed wrong class function
    Signed-off-by: John Rodriguez <syntheticgio@aol.com>

--- server/server.py
+++ server/server.py
@@ -555,7 +555,7 @@ class SubmitJobHandler(MainHandler):
         print("Body: {}".format(json_body))
         print("Checking to see if {} is available to push to computations.".format(json_body['tcga_id']))
         try:
-            if self.batch_scriptor.s.generate_sbatch_by_tcga_id(json_body['tcga_id']):
+            if self.batch_scriptor.generate_sbatch_by_tcga_id(json_body['tcga_id']):
                 # Remove this tcga ID entry from the query table, and put it into the processing one
                 sql_statement = """
                                 INSERT INTO
