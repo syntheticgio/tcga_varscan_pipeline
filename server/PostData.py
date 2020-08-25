793e176de0583f14c5950fc7e395b1693815a1c2
John Rodriguez <syntheticgio@aol.com>
2020-08-25 07:52:14 -0400

    Capture POST error
    Signed-off-by: John Rodriguez <syntheticgio@aol.com>

--- server/PostData.py
+++ server/PostData.py
@@ -4,6 +4,9 @@ import json
 
 
 def PostData(wgs_url, wgs_data):
-    r = requests.post(wgs_url, data=json.dumps(wgs_data))
-    print("POSTed data, return value: {}".format(r.status_code))
-    print("POSTed data, return : {}".format(r.text))
+    try:
+        r = requests.post(wgs_url, data=json.dumps(wgs_data))
+        print("POSTed data, return value: {}".format(r.status_code))
+        print("POSTed data, return : {}".format(r.text))
+    except requests.exceptions.ConnectionError as e:
+        print("ERROR: %e", e)
