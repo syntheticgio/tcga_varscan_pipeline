116ec241601367c5f278d81645d3657584c90f6f
John Torcivia <torcivia@gwu.edu>
2020-09-06 01:50:18 +0000

    added check for GiB

--- src/generate_finished.sh
+++ src/generate_finished.sh
@@ -5,16 +5,18 @@ passed=0
 failed=0
 
 rm -f finished_tcga_ids.txt
+rm -f failed_ids.txt
 
 gsutil ls gs://iron-eye-6998/tcga_wgs_results | awk 'BEGIN{FS="/";}{if (NR > 1) {print $5;}}' >get_ids.txt
 
 while IFS='' read -r TCGA || [[ -n "$TCGA" ]]; do
 	b=`gsutil ls -l gs://iron-eye-6998/tcga_wgs_results/${TCGA}/varscan_results/TCGA* | awk '{FS="("; if(NR > 1) {print $2;}}' | awk '{FS=" "; gsub(")",""); print $2;}'`
-	if [[ "${b}" == "MiB" ]]; then
+	if [[ "${b}" == "MiB" ]] || [[ "${b}" == "GiB" ]]; then
 		echo "${TCGA}" >> finished_tcga_ids.txt
 		passed=$((passed+1))
 	else
 		failed=$((failed+1))
+                echo "${TCGA}" >> failed_ids.txt
 	fi 
 i=$((i+1))
 echo -ne "P: ${passed} / F: ${failed}"\\r
