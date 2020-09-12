0cda7187ace966681ec84555f3a197428cefa38c
John Rodriguez <syntheticgio@aol.com>
2020-09-12 11:28:24 -0400

    Fixed issue where additional files broke getting finished list.
    Signed-off-by: John Rodriguez <syntheticgio@aol.com>

--- src/generate_finished.sh
+++ src/generate_finished.sh
@@ -10,7 +10,7 @@ rm -f failed_ids.txt
 gsutil ls gs://iron-eye-6998/tcga_wgs_results | awk 'BEGIN{FS="/";}{if (NR > 1) {print $5;}}' >get_ids.txt
 
 while IFS='' read -r TCGA || [[ -n "$TCGA" ]]; do
-	b=`gsutil ls -l gs://iron-eye-6998/tcga_wgs_results/${TCGA}/varscan_results/TCGA* | awk '{FS="("; if(NR > 1) {print $2;}}' | awk '{FS=" "; gsub(")",""); print $2;}'`
+	b=`gsutil ls -l gs://iron-eye-6998/tcga_wgs_results/${TCGA}/varscan_results/TCGA*.vcf.tar.gz | awk '{FS="("; if(NR > 1) {print $2;}}' | awk '{FS=" "; gsub(")",""); print $2;}'`
 	if [[ "${b}" == "MiB" ]] || [[ "${b}" == "GiB" ]]; then
 		passed=$((passed+1))
 		echo "${TCGA}" >> finished_tcga_ids.txt
